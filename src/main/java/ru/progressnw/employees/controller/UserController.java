984a8efa2bbda2ca311d1703e86cedc604f92025
aleksandr.budimirov@telekom.com <aleksandr.budimirov@telekom.com>
2021-11-30 22:31:10 +0300

    fix of NPE

--- src/main/java/ru/progressnw/employees/controller/UserController.java
+++ src/main/java/ru/progressnw/employees/controller/UserController.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class UserController {
 
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/registration")
