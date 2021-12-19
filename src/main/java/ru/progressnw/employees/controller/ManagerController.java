d3815a59e10bf6017cf827ba8118a07109a48234
aleksandr.budimirov@telekom.com <aleksandr.budimirov@telekom.com>
2021-12-19 20:14:09 +0300

    add list of departments to view

--- src/main/java/ru/progressnw/employees/controller/ManagerController.java
+++ src/main/java/ru/progressnw/employees/controller/ManagerController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.progressnw.employees.model.Department;
 import ru.progressnw.employees.model.User;
 import ru.progressnw.employees.repository.UserRepository;
+import ru.progressnw.employees.service.DepartmentService;
 import ru.progressnw.employees.service.ResponsibilityService;
 import ru.progressnw.employees.service.UserService;
 
@@ -18,11 +20,13 @@ public class ManagerController {
     private final UserRepository userRepository;
     private final UserService userService;
     private final ResponsibilityService responsibilityService;
+    private final DepartmentService departmentService;
 
     @GetMapping("/manager")
     public String showUserList(Model model) {
         User manager = userRepository.findByUsername(userService.getLoggedUsername());
         List<User> usersList = userService.getUsersByManager(manager);
+        model.addAttribute("managedDepartments", departmentService.getDepartmentListByManager(manager));
         model.addAttribute("users", usersList);
         model.addAttribute("responsibilities", responsibilityService.getResponsibilityListByUsers(usersList));
         return "manager";
